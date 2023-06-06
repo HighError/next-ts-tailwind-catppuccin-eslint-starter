ca348ce47e43f733067cb529a39f37bee8eaab09
HighError <hierrorua@gmail.com>
2023-06-06 11:53:37 +0300

    feat: 🎸 Added all settings

---
+++ app/providers.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { ThemeProvider } from 'next-themes';
+import React, { ReactNode, useEffect, useState } from 'react';
+
+export default function Providers({ children }: { children: ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
+  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+}
